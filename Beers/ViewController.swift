30bbeec83326cd54818d7dd60121d89b0412eccf
Olalla <olallagzreyes@gmail.com>
2022-06-08 03:47:09 -0700

    Initial Commit

---
+++ Beers/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Beers
+//
+//  Created by Olalla Gómez Reyes on 8/6/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
